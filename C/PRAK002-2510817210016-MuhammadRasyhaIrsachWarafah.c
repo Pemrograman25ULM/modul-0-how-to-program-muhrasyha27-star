66f40ea6796ace736edcd12921012cffb7873bde
irsak <muh.rasyha27@gmail.com>
2025-09-29 21:50:10 +0800

    Add files via upload

---
+++ C/PRAK002-2510817210016-MuhammadRasyhaIrsachWarafah.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    printf("Selamat Pagi, irsak\nSelamat siang, irsak\nSelamat Malam, irsak\n");
+
+    return 0;
+}
---
+++ C/PRAK004-2510817210016-MuhammadRasyhaIrsachWarafah.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    printf("###########################################\n");
+    printf("#                                         #\n");
+    printf("#     Muhammad Rasyha Irsach Warafah      #\n");
+    printf("#             2510817210016               #\n");
+    printf("#                                         #\n");
+    printf("###########################################\n");
+
+    return 0;
+}
